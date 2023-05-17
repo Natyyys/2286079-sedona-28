c95224628eb7e3a7b01de67638df3df5c28963c2
Наталья Моисеева <moiseeva_n_s@inbox.ru>
2023-05-17 22:57:11 +0300

    Меняет последовательность исполнения задач (sprite)

--- gulpfile.js
+++ gulpfile.js
@@ -40,49 +40,49 @@ const html = () => {
 
 const scripts = () => {
   return gulp.src('source/js/*.js')
-  .pipe(terser())
-  .pipe(gulp.dest('build/js'));
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'));
 }
 
 //Images
 
 const optimizeImages = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
-  .pipe(squoosh())
-  .pipe(gulp.dest('build/img'));
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'));
 }
 
 const copyImages = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
-  .pipe(gulp.dest('build/img'));
+    .pipe(gulp.dest('build/img'));
 }
 
 //WebP
 
 const createWebp = () => {
   return gulp.src('source/img/**/*.{jpg,png}')
-  .pipe(squoosh({
+    .pipe(squoosh({
       webp: {}
-  }))
-  .pipe(gulp.dest('build/img'));
+    }))
+    .pipe(gulp.dest('build/img'));
 }
 
 //SVG
 
 const svg = () => {
   return gulp.src('source/img/*.svg')
-  .pipe(svgo())
-  .pipe(gulp.dest('build/img'));
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
 }
 
 const sprite = () => {
   return gulp.src('source/img/icons/*.svg')
-  .pipe(svgo())
-  .pipe(svgstore({
-    inlineSvg: true
-  }))
-  .pipe(rename('sprite.svg'))
-  .pipe(gulp.dest('build/img'));
+    .pipe(svgo())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
 }
 
 //Copy
@@ -145,9 +145,9 @@ export const build = gulp.series(
     html,
     scripts,
     svg,
-    sprite,
     createWebp
   ),
+  sprite,
 );
 
 //Default
@@ -161,9 +161,9 @@ export default gulp.series(
     html,
     scripts,
     svg,
-    sprite,
     createWebp
   ),
+  sprite,
   gulp.series(
     server,
     watcher
