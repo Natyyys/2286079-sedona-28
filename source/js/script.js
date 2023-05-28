f49a20014404eb82175f6b6f21b4eeefee52fb67
Наталья Моисеева <moiseeva_n_s@inbox.ru>
2023-05-28 21:33:41 +0300

    Исправляет меню при отключенгом js

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,17 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+const CLASS_NAV_OPEN = 'main-nav--opened';
+const CLASS_NAV_JS = 'main-nav--js';
 
-navMain.classList.remove('main-nav--nojs');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+document.addEventListener('DOMContentLoaded', () => {
+  if (!(navMain && navToggle)) {
+    return;
   }
+
+  navMain.classList.add(CLASS_NAV_JS);
+
+  navToggle.addEventListener('click', () => {
+    navMain.classList.toggle(CLASS_NAV_OPEN);
+  });
 });
